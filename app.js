c3b0b2ce56ecc3472b2d0c006cc2cc4e7249aef1
davidbgemin7357 <davidbgemin.dev@gmail.com>
2023-06-03 23:56:31 -0500

    dev

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 class Prueba {
-    
     constructor() {
         this.init();
         this.employees = [];
@@ -10,68 +9,123 @@ class Prueba {
             await this.initDataServer();
             this.devex();
         } catch (error) {
-            console.log(error);            
+            console.log(error);
         }
     }
 
-    
     devex() {
         this.dx = {
             gridEmployees: null,
-        }
-        
+            mvTabs: null,
+            tabGrids: null,
+            tbarMv: null,
+        };
 
-        this.dx.gridEmployees = $('<div>')
-        .dxDataGrid({
-            dataSource: this.employees,
-            // keyExpr: 'employeeId',
-            showColumnLines: true,
-            rowAlternationEnabled: true,
-            wordWrapEnabled: true,
-            searchPanel: {
-                visible: true,
-                width: 240,
-                placeholder: 'Buscar...'
-            }
-        }).dxDataGrid('instance')
+        this.dx.tbarMv = $('<div>')
+        .dxToolbar({
+            items: [
+                {
+                    location: 'before',
+                    text: 'Hola'
+                },
+                {
+                    location: 'after',
+                    widget: 'dxButton',
+                    options: {
+                        text: 'Siguiente vista vista',
+                        hint: 'Mostrar siguiente vista',
+                        icon: 'check',
+                        type: 'success',
 
-        $('#contenedor')
-        .addClass('container')
-        .append(
-            this.dx.gridEmployees.element(),
+                    }
+                }
+            ]
+        }).dxToolbar('instance')
+
+        this.dx.gridEmployees = $("<div>")
+            .dxDataGrid({
+                dataSource: this.employees,
+                // keyExpr: 'employeeId',
+                showColumnLines: true,
+                rowAlternationEnabled: true,
+                wordWrapEnabled: true,
+                searchPanel: {
+                    visible: true,
+                    width: 240,
+                    placeholder: "Buscar...",
+                },
+            })
+            .dxDataGrid("instance");
+
+        this.dx.tabGrids = $("<div>")
+            .dxTabPanel({
+                selectedIndex: 0,
+                animationEnabled: true,
+                loop: false,
+                items: [
+                    {
+                        title: 'Empleados',
+                        icon: 'user',
+                        template: (itemData, itemIndex, element) => {
+                            element
+                                .append(
+                                    this.dx.gridEmployees.element(),
+                                );
+                        }
+                    }
+                ],
+            })
+            .dxTabPanel("instance");
+
+        this.dx.mvTabs = $("<div>")
+            .dxMultiView({
+                selectedIndex: 0,
+                loop: false,
+                animationEnabled: true,
+                swipeEnabled: true,
+                items: [
+                    {
+                        template: (itemData, itemIndex, element) => {
+                            element.append(
+                                // this.dx.tabGrids.element()
+                                this.dx.tbarMv.element(),
+                                this.dx.gridEmployees.element()
+                            )
+                        },
+                    },
+                ],
+            })
+            .dxMultiView("instance");
+
+        $("#contenedor").addClass("container").append(
+            this.dx.mvTabs.element()
         );
     }
 
-
-
     async initDataServer() {
         try {
-            
-            // const response = await fetch("https://reqres.in/api/users?page=2")
-            const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0")
-            debugger;
-            if(!response.ok) throw new Error('Error en la solicitud');
-            this.employees = (await response.json()).results;
-            const name = "David"
-            // .map(p => {
-            //     return {
-            //         employeeId: p.id,
-            //         picture: p.avatar,
-            //         email: p.email,
-            //         name: p.first_name,
-            //         lastname: p.last_name,
-            //     }
-            // });
-
+            const response = await fetch(
+                "https://randomuser.me/api/?results=40"
+            );
+            if (!response.ok) throw new Error("Error en la solicitud");
+            this.employees = (await response.json()).results.map((p) => {
+                return {
+                    name: p.name.first + " " + p.name.last,
+                    email: p.email,
+                    gender: p.gender,
+                    id: p.id.value,
+                    phone: p.cell,
+                    country: p.location.country,
+                    photo: p.picture.large,
+                    age: p.dob.age,
+                    username: p.login.username,
+                };
+            });
         } catch (error) {
-            console.log(error)
+            console.log(error);
         }
     }
-
-
 }
-
-
 
 window.onload = function () {
     try {
@@ -79,4 +133,4 @@ window.onload = function () {
     } catch (err) {
         console.warn(err);
     }
-}
+};
